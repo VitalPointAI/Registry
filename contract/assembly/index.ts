32dad2dbacef4ddf74ed920782e1c0d933740661
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-07-28 15:10:02 -0400

    cleaned up for audit

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,30 +1,21 @@
-import { PersistentMap, PersistentSet, env, storage, logging, Context, ContractPromiseBatch, base58, u128 } from 'near-sdk-as'
+import { PersistentMap, env, Context } from 'near-sdk-as'
 
-const registry = new PersistentMap<string, string>('r')
-const schemas = new PersistentSet<string>('s')
-const definitions = new PersistentSet<string>('d')
-const aliases = new PersistentSet<string>('a')
+const didRegistry = new PersistentMap<string, string>('dR')
+const aliasRegistry = new PersistentMap<string, string>('aR')
 
-const allAliases = new PersistentMap<string, string>('l')
-
-//DID REGISTRY
-/**
-* Returns the app owner which we use in multiple places to confirm user can override/update definitions 
-* @param owner 
-*/
-function _isOwner(): boolean {
-  let owner = storage.getSome<string>("owner")
-  return owner == Context.predecessor
-}
+//DID Registry
+//
+// accountId: someaccount e.g. vitalpointai.near
+// did: decentralized identifier e.g., did:key:z6Mkw1Mpfejq2R76AsQo2qJoAVaF6HH5nLDoHrKrsW5Wdnei
 
 export function getDID(accountId: string) : string {
   assert(env.isValidAccountID(accountId), 'not a valid near account')
-  assert(registry.contains(accountId), 'no did registered for this near account')
-  return registry.getSome(accountId)
+  assert(didRegistry.contains(accountId), 'no did registered for this near account')
+  return didRegistry.getSome(accountId)
 }
 
 export function hasDID(accountId: string) : bool {
-  if(registry.contains(accountId)){
+  if(didRegistry.contains(accountId)){
     return true
   } else {
     return false
@@ -32,20 +23,32 @@ export function hasDID(accountId: string) : bool {
 }
 
 export function putDID(accountId: string, did: string): bool {
-  assert(Context.sender == Context.predecessor, 'only account owner can register or change a DID for it')
+  assert(Context.sender == Context.predecessor, 'only account owner can register or change their associated DID in the registry')
   assert(env.isValidAccountID(accountId), 'not a valid near account')
-  registry.set(accountId, did)
+  didRegistry.set(accountId, did)
   return true
 }
 
-// NEW ALIAS TRACKING
+export function deleteDID(accountId: string): bool {
+  assert(Context.sender == Context.predecessor, 'only account owner can delete their DID from the registry')
+  assert(env.isValidAccountID(accountId), 'not a valid NEAR account')
+  didRegistry.delete(accountId)
+  return true
+}
+
+// ALIAS and Definition Registry
+// 
+// alias: accountId +':'+ aliasName  e.g. vitalpointai.near:profileData
+// definition: a StreamId of a Ceramic definition id e.g. kjzl6cwe1jw14bdsytwychcd91fcc7xibfj8bc0r2h3w5wm8t6rt4dtlrotl1ou
+// definition contains its name, description, and schema
+
 export function retrieveAlias(alias: string) : string {
-  assert(allAliases.contains(alias), 'alias does not exist')
-  return allAliases.getSome(alias)
+  assert(aliasRegistry.contains(alias), 'alias is not registered')
+  return aliasRegistry.getSome(alias)
 }
 
 export function hasAlias(alias: string) : bool {
-  if(allAliases.contains(alias)){
+  if(aliasRegistry.contains(alias)){
     return true
   } else {
     return false
@@ -53,82 +56,14 @@ export function hasAlias(alias: string) : bool {
 }
 
 export function storeAlias(alias: string, definition: string): bool {
-  assert(!allAliases.contains(alias), 'alias already exists')
-  allAliases.set(alias, definition)
-  logging.log('registered alias' + alias + ':' + definition)
+  assert(!aliasRegistry.contains(alias), 'alias already registered')
+  aliasRegistry.set(alias, definition)
   return true
 }
 
-export function changeDefinition(alias: string, definition: string): bool {
-  storage.set<string>('owner', 'vitalpointai.testnet')
-  assert(_isOwner(), 'not the owner')
-  allAliases.set(alias, definition)
-  logging.log('updated alias' + alias + ':' + definition)
+export function deleteAlias(owner: string, alias: string): bool {
+  assert(owner == Context.predecessor, 'only alias owner can delete if from the registry')
+  assert(aliasRegistry.contains(alias), 'alias is not registered')
+  aliasRegistry.delete(alias)
   return true
 }
-
-export function initialize(): void {
-  let promise = ContractPromiseBatch.create(Context.contractName)
-  .add_access_key(
-    base58.decode(Context.senderPublicKey),
-    new u128(250),
-    Context.contractName,
-    ['storeAlias', 'putDID'],
-    0)
-  storage.set<bool>('done', true)
-}
-
-export function getInitialize(): bool {
-  if (storage.contains('done')) {
-    if(storage.getSome<bool>('done') == true ) {
-      return true
-    } else {
-      return false
-    }
-  }
-  return false
-}
-
-// Schemas
-export function getSchemas(): Array<string> {
-  return schemas.values()
-}
-
-export function findSchema(schema: string): bool {
-  return schemas.has(schema)
-}
-
-export function addSchema(schema: string): bool {
-  schemas.add(schema)
-  return true
-}
-
-// Aliases
-export function getAliases(): Array<string> {
-  return aliases.values()
-}
-
-export function findAlias(alias: string): bool {
-  return aliases.has(alias)
-}
-
-export function addAlias(alias: string): bool {
-  aliases.add(alias)
-  return true
-}
-
-// Definitions
-export function getDefinitions(): Array<string> {
-  return definitions.values()
-}
-
-export function findDefinition(def: string): bool {
-  return definitions.has(def)
-}
-
-export function addDefinition(def: string): bool {
-  definitions.add(def)
-  return true
-}
-
-
